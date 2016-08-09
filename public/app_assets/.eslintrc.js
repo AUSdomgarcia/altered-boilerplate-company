bdb4dc6d1062799e585626377843ccc5580862d8
Michael Yap <michael.yap@nuworks.ph>
2016-08-09 16:19:14 +0800

    Added eslint rules

--- public/app_assets/.eslintrc.js
+++ public/app_assets/.eslintrc.js
@@ -8,7 +8,17 @@ module.exports = {
   "rules": {
     "quotes": [2,"single"],
     "no-new-object": 2,
+    "no-array-constructor": 2,
     "no-inner-declarations": 2,
-    "dot-notation": 2
+    "dot-notation": 2,
+    "vars-on-top": 2,
+    "one-var": [2, "never"],
+    "eqeqeq": 2,
+    "brace-style": 2,
+    "key-spacing": [2, { "beforeColon": false }],
+    "comma-style": [2, "last"],
+    "no-extra-semi": 2,
+    "semi": 2,
+    "strict": [2, "function"]
   }
 };
